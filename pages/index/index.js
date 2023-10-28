a8efd46290bebb8605c21ffbeffa446ff3a7388e
Endlin Boeingstein <endlinboeingstein@gmail.com>
2023-10-28 21:27:29 +0800

    36集

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,45 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    info:'hello world',
+    imgSrc:'http://www.itheima.com/images/logo.png',
+    randomNum1:Math.random()*10,
+    randomNum2:Math.random().toFixed(2),
+    count:0,
+    msg:'你好',
+    type:1,
+    flag:true,
+    arr1:['苹果','华为','小米'],
+    userList:[
+      {id:1,name:'小红'},
+      {id:2,name:'小黄'},
+      {id:3,name:'小白'}
+    ]
   },
+
+  btnTapHandler(e){
+    console.log(e);
+  },
+
+  CountChange(){
+    this.setData({
+      count:this.data.count+1
+    })
+  },
+
+  btnTap2(e){
+    this.setData({
+      count:this.data.count+e.target.dataset.info
+    })
+  },
+
+  inputHandler(e){
+    //console.log(e.detail.value)
+    this.setData({
+      msg:e.detail.value
+    })
+  },
+
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
